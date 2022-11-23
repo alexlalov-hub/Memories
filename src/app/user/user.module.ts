56192161ba66ebe6f01d1a350d405efe81faa8ce
Aleksandar Lalov <alex.lalov04@gmail.com>
2022-11-23 20:08:01 +0200

    Moving the authentication modal to its own component and starting work on it

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthenticationModalComponent } from './authentication-modal/authentication-modal.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        AuthenticationModalComponent
+    ],
+    exports: [
+        AuthenticationModalComponent
+    ],
+    imports: [
+        CommonModule
+    ]
 })
 export class UserModule { }
