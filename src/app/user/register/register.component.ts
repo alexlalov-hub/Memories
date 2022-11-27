22247400381751346cc65d2168718080f5eceb40
Aleksandar Lalov <alex.lalov04@gmail.com>
2022-11-27 11:30:53 +0200

    Adding form validation and error handling on client side

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormGroup, FormControl} from "@angular/forms";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-register',
@@ -8,11 +8,31 @@ import {FormGroup, FormControl} from "@angular/forms";
 })
 export class RegisterComponent {
     registerForm = new FormGroup({
-        name: new FormControl(''),
-        email: new FormControl(''),
-        age: new FormControl(''),
-        password: new FormControl(''),
-        confirmPassword: new FormControl(''),
-        phoneNumber: new FormControl('')
+        name: new FormControl('', [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(16),
+        ]),
+        email: new FormControl('', [
+            Validators.required,
+            Validators.email
+        ]),
+        age: new FormControl('', [
+            Validators.required,
+            Validators.min(18),
+            Validators.max(120)
+        ]),
+        password: new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+        ]),
+        confirmPassword: new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+        ]),
+        phoneNumber: new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^[0-9]{10}$/)
+        ])
     })
 }
