67b5e804c3789c7ab6d1f193c206bd465cee9641
Aleksandar Lalov <alex.lalov04@gmail.com>
2022-12-09 20:13:15 +0200

    Adding logic for logging out

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ModalService} from "../services/modal.service";
 import {AuthService} from "../services/auth.service";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
     selector: 'app-nav',
@@ -9,7 +10,7 @@ import {AuthService} from "../services/auth.service";
 })
 export class NavComponent implements OnInit {
 
-    constructor(public modal: ModalService, public auth: AuthService) {
+    constructor(public modal: ModalService, public auth: AuthService,private firebase: AngularFireAuth) {
     }
 
     ngOnInit(): void {
@@ -19,5 +20,11 @@ export class NavComponent implements OnInit {
         $event.preventDefault()
 
         this.modal.toggleModal('auth')
+    }
+
+    async logout($event: Event){
+        $event.preventDefault()
+
+        await this.firebase.signOut()
     }
 }
