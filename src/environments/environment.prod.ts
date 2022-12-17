665ad10159a95c86546f7111be766d04b69bf2f2
Aleksandar Lalov <alex.lalov04@gmail.com>
2022-12-17 15:12:13 +0200

    Updating the file for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-    production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyDI7gpqiaDWukvBv-dMpbn0nvG5rAoFvRQ",
+        authDomain: "memories-ba98a.firebaseapp.com",
+        projectId: "memories-ba98a",
+        storageBucket: "memories-ba98a.appspot.com",
+        messagingSenderId: "447958303780",
+        appId: "1:447958303780:web:ef0a9a0bd9745fa2b504b1"
+    }
 };
